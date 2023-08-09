e64de709b6eba37f094168a8569588b7d93d144c
vhtoledo <querynetworking@gmail.com>
2023-08-09 12:50:34 -0300

    creacion de datos

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 26.99},
+}
+
 func main() {
 	router := gin.Default()
 

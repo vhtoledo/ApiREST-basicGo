3da5f651df67e1607120f34a617325f6c5c55b75
vhtoledo <querynetworking@gmail.com>
2023-08-09 12:45:09 -0300

    instalacion de gin y correr servidor

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Bienvenidos")
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hola Mundo",
+		})
+	})
+
+	router.Run("localhost:8080")
 }

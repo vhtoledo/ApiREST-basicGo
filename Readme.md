e29b3901623d24127da251f2cf29e61e3cf54000
vhtoledo <querynetworking@gmail.com>
2023-08-10 09:50:49 -0300

    controlador para devolver elementos por id

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# ApiRest Basico
+"""API RESTful utilizando el lenguaje de programación Go y el framework Gin"""
+
+# Ejecutar 
+""" go run . """
--- main.go
+++ main.go
@@ -37,11 +37,26 @@ func postAlbums(c *gin.Context){
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+//Funcion para devolver la data por id
+func getAlbumsByID(c *gin.Context){
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album no encontrado"})
+}
+
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/albums", getAlbums);
+	router.GET("/albums/:id", getAlbumsByID);
 	router.POST("/albums", postAlbums);
 
 	router.Run("localhost:8080")
